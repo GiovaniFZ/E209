d14a3005f76fe006ac03a26a1bbe5f0bdbed5980
Giv <giovanifinazzi@gmail.com>
2023-03-04 13:26:01 -0300

    Blink led

---
+++ Programa_Piscar_led.ino
@@ -0,0 +1,13 @@
+int main (void){
+  DDRD = 0x03;
+  while(1){
+    PORTD = 0x01;
+    _delay_ms(1000);
+    PORTD = 0x02;
+    _delay_ms(1000);
+    PORTD = 0x03;
+    _delay_ms(1000);
+    PORTD = 0x00;
+    _delay_ms(1000);    
+  }
+}
